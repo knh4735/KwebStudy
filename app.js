a3eb1705ee481cb7a3950cf0101df405e21146da
Nagion <skrgus2004@gmail.com>
2019-06-04 16:30:30 +0900

    Study GraphQL with pokemon DB

--- app.js
+++ app.js
@@ -4,21 +4,55 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var graphqlHTTP = require('express-graphql');
+var { buildSchema } = require('graphql');
+var pokedex = require('./pokedex');
 
-var routes = require('./routes/index');
+var pokeSchema = buildSchema(`
+      type Pokemon {
+        no: Int
+        name : String
+        type : String
+        expl : String
+      }
+    
+      type Query {
+        pokemon(no : Int!) : Pokemon
+      }
+    `);
+
+class Pokemon {
+  constructor(pokemon) {
+    this.no = pokemon.no;
+    this.name = pokemon.name;
+    this.type = pokemon.type;
+    this.expl = pokemon.expl;
+  }
+
+  no() {return no;}
+  name() {return name;}
+  type() {return type;}
+  expl() {return expl;}
+}
+
+var root = {
+  pokemon: function ({no}) {
+    return new Pokemon(pokedex[no]);
+  }
+};
+
+
+var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
 
-var mongoose = require("mongoose");
-var db = mongoose.connection;
-db.on('error', console.error);
-db.once('open', function(){
-  // CONNECTED TO MONGODB SERVER
-  console.log("Connected to mongod server");
-});
 
-mongoose.connect('mongodb://localhost:27017');
+app.use('/graphql', graphqlHTTP({
+  schema: pokeSchema,
+  rootValue: root,
+  graphiql: true,
+}));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -28,11 +62,11 @@ app.set('view engine', 'ejs');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
+app.use('/', index);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
@@ -42,31 +76,15 @@ app.use(function(req, res, next) {
   next(err);
 });
 
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
+// error handler
 app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
   res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+  res.render('error');
 });
 
 module.exports = app;
-
-//brew services start mongodb
-//brew services stop mongodb
