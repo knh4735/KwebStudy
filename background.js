3d7123c441b02a30f2fed489365676c68fe0911d
Nagion <skrgus2004@naver.com>
2019-09-04 14:50:02 +0900

    Add files via upload

--- background.js
+++ background.js
@@ -1,280 +1,310 @@
-const kulmsURL = "https://kulms.korea.ac.kr/webapps/portal/execute/tabs/tabAction?tab_tab_group_id=_2_1";
+const kulmsURL =
+  "https://kulms.korea.ac.kr/webapps/portal/execute/tabs/tabAction?tab_tab_group_id=_2_1";
 
-const init = ()=>{
-	//Loading sign on badge
-	chrome.browserAction.setBadgeText({text: '...'});
-	chrome.browserAction.setBadgeBackgroundColor({color: "#888800"});
+const init = () => {
+  //Loading sign on badge
+  chrome.browserAction.setBadgeText({ text: "..." });
+  chrome.browserAction.setBadgeBackgroundColor({ color: "#888800" });
 
-	// Regularly Access (30 minutes) to Blackboard and get lecture diff
-	access();
-	let accessInterval = setInterval(access, INTERVAL_MINUTES(30));
+  // Regularly Access (30 minutes) to Blackboard and get lecture diff
+  access();
+  let accessInterval = setInterval(access, INTERVAL_MINUTES(30));
 
-	chrome.runtime.onMessage.addListener(msgListener);
+  chrome.runtime.onMessage.addListener(msgListener);
 };
 
 const INTERVAL_MINUTES = minutes => 1000 * 60 * minutes;
 
 const isLoggedIn = data => data.indexOf("Go to Login page") === -1;
 
-const makeLoginForm = (id, pw) => `id=${id}&pw=${pw}&returnURL=kulms.korea.ac.kr`;
+const makeLoginForm = (id, pw) =>
+  `id=${id}&pw=${pw}&returnURL=kulms.korea.ac.kr`;
 
-const loginBlackboard = async (id, pw)=>{
-	const loginData = makeLoginForm(id, pw);
-	const loginURL = "https://kulms.korea.ac.kr/auth-saml/saml/login?apId=_147_1&redirectUrl=https%3A%2F%2Fkulms.korea.ac.kr%2Fwebapps%2Fportal%2Fexecute%2FdefaultTab";
+const loginBlackboard = async (id, pw) => {
+  const loginData = makeLoginForm(id, pw);
+  const loginURL =
+    "https://kulms.korea.ac.kr/auth-saml/saml/login?apId=_147_1&redirectUrl=https%3A%2F%2Fkulms.korea.ac.kr%2Fwebapps%2Fportal%2Fexecute%2FdefaultTab";
 
-	try{
-		const beforeLoginPage = await $.get(loginURL);
-		const beforeLoginPageForm = $("<div></div>").append($.parseHTML(beforeLoginPage)).find("form");
+  try {
+    const beforeLoginPage = await $.get(loginURL);
+    const beforeLoginPageForm = $("<div></div>")
+      .append($.parseHTML(beforeLoginPage))
+      .find("form");
 
-		const loginPage = await $.post(beforeLoginPageForm.attr("action"), beforeLoginPageForm.serialize());
-		const loginForm = $("<div></div>").append($.parseHTML(loginPage)).find("form");
+    const loginPage = await $.post(
+      beforeLoginPageForm.attr("action"),
+      beforeLoginPageForm.serialize()
+    );
+    const loginForm = $("<div></div>")
+      .append($.parseHTML(loginPage))
+      .find("form");
 
-		loginForm.find("input[name='user_id']").remove();
-		loginForm.find("input[name='user_password']").remove();
-		loginForm.find("input[name='user_timezone_offset']").val(new Date().getTimezoneOffset());
-		loginForm.append("<input name='user_id' value='"+id+"'/>");
-		loginForm.append("<input name='user_password' value='"+pw+"'/>");
+    loginForm.find("input[name='user_id']").remove();
+    loginForm.find("input[name='user_password']").remove();
+    loginForm
+      .find("input[name='user_timezone_offset']")
+      .val(new Date().getTimezoneOffset());
+    loginForm.append("<input name='user_id' value='" + id + "'/>");
+    loginForm.append("<input name='user_password' value='" + pw + "'/>");
 
-		const tokenPage = await $.post("https://sso.korea.ac.kr"+loginForm.attr("action"), loginForm.serialize());
-		const tokenPageForm = $("<div></div>").append($.parseHTML(tokenPage)).find("form");
-		
-		const response = await $.post(tokenPageForm.attr("action"), tokenPageForm.serialize());
+    const tokenPage = await $.post(
+      "https://sso.korea.ac.kr" + loginForm.attr("action"),
+      loginForm.serialize()
+    );
+    const tokenPageForm = $("<div></div>")
+      .append($.parseHTML(tokenPage))
+      .find("form");
 
-		return Promise.resolve(isLoggedIn(response));
-	}
-	catch(err){
-		console.log(err);
-		return false;
-	}
+    const response = await $.post(
+      tokenPageForm.attr("action"),
+      tokenPageForm.serialize()
+    );
+
+    return Promise.resolve(isLoggedIn(response));
+  } catch (err) {
+    console.log(err);
+    return false;
+  }
 };
 
-const reqBlackboard = async ()=>{
-	try{
-		const data = await $.get(kulmsURL);
-		return Promise.resolve(data);
-	}
-	catch(err){
-		console.log(err);
-		return false;
-	}
+const reqBlackboard = async () => {
+  try {
+    const data = await $.get(kulmsURL);
+    return Promise.resolve(data);
+  } catch (err) {
+    console.log(err);
+    return false;
+  }
 };
 
-const getNumberOfEachContents = async (url)=>{
-	const data = await $.get(url);
-	const numberOfContents = $("<div></div>").append($.parseHTML(data)).find("#content ul:not(.clearfix)[class]").children().length;
+const getNumberOfEachContents = async url => {
+  const data = await $.get(url);
+  const numberOfContents = $("<div></div>")
+    .append($.parseHTML(data))
+    .find("#content ul:not(.clearfix)[class]")
+    .children().length;
 
-	return Promise.resolve(numberOfContents);
+  return Promise.resolve(numberOfContents);
 };
 
-const getNumberOfContents = async (lectureURLs)=>{
-	let contents = {};
-	for(let type in lectureURLs){
-		const num = await getNumberOfEachContents(lectureURLs[type]);
-		contents[type] = num;
-	}
+const getNumberOfContents = async lectureURLs => {
+  let contents = {};
 
-	return contents;
+  for (let type in lectureURLs) {
+    const num = await getNumberOfEachContents(lectureURLs[type]);
+    contents[type] = num;
+  }
+
+  return contents;
 };
 
-const setLectureAlarm = async (lectures)=>{
-	return await Promise.all(
-		lectures.map(async (lecture)=>{
-			const newLecture = {
-				...lecture,
-				alarm: await getNumberOfContents(lecture.URL)
-			};
-			return newLecture;	
-		})
-	);
+const setLectureAlarm = async lectures => {
+  console.log(lectures);
+
+  return await Promise.all(
+    lectures.map(async lecture => {
+      const newLecture = {
+        ...lecture,
+        alarm: await getNumberOfContents(lecture.URL)
+      };
+      return newLecture;
+    })
+  );
 };
 
-const updateBadge = (lectures)=>{
-	let alarms = 0;
+const updateBadge = lectures => {
+  let alarms = 0;
 
-	lectures.forEach((lecture)=>{
-		for(let type in lecture.alarm)
-			if(lecture.alarm[type] > lecture.record[type])
-				alarms += lecture.alarm[type] - lecture.record[type];
-	});
+  lectures.forEach(lecture => {
+    for (let type in lecture.alarm)
+      if (lecture.alarm[type] > lecture.record[type])
+        alarms += lecture.alarm[type] - lecture.record[type];
+  });
 
-	chrome.browserAction.setBadgeText({text: `${alarms}`});
-	if(alarms > 0)
-		chrome.browserAction.setBadgeBackgroundColor({color: "#ff0000"});
-	else
-		chrome.browserAction.setBadgeBackgroundColor({color: "#0000ff"});
-}
-
-const updateLectureAlarm = async (lectures)=>{
-	const newLecture = await setLectureAlarm(lectures);
-
-	chrome.storage.local.set({lecture: newLecture});
-
-	updateBadge(newLecture);
+  chrome.browserAction.setBadgeText({ text: `${alarms}` });
+  if (alarms > 0)
+    chrome.browserAction.setBadgeBackgroundColor({ color: "#ff0000" });
+  else chrome.browserAction.setBadgeBackgroundColor({ color: "#0000ff" });
 };
 
-const access = async ()=>{
-	const data = await reqBlackboard();
-	if(!data) return;
+const updateLectureAlarm = async lectures => {
+  const newLecture = await setLectureAlarm(lectures);
 
-	// GET INFORMATIONS IF SET LECTURES
-	// OTHERWISE GET LECTURES AND URL
-	// THEN SHOW 
-	if(isLoggedIn(data)) {
-		console.log("Logged In");
+  chrome.storage.local.set({ lecture: newLecture });
 
-		chrome.storage.local.set({"available": true});
-
-		chrome.storage.local.get(['lecture'], (result)=>{
-			if(result.lecture)				// IF lecture URL is set then Parse url
-				updateLectureAlarm(result.lecture);
-		
-			else{								// IF lecture URL is not set
-				chrome.tabs.create({url: kulmsURL});
-				chrome.tabs.create({url:"set_lecture.html"});
-			}
-		});
-	}
-	else {										// Login and Re-Access
-		console.log("Login Needed");
-
-		chrome.storage.local.get(['account', 'available'], async function(result){
-			if(result.account && result.available){				//IF Account is saved & Available, Use it
-				let account = result.account;
-				let id, pw;
-
-				id = account.id;
-				pw = CryptoJS.AES.decrypt(account.pw, id).toString(CryptoJS.enc.Utf8);
-
-				const loggedIn = await loginBlackboard(id, pw);
-
-				if(loggedIn)
-					access();
-				else
-					chrome.tabs.create({url:"login.html"});
-			}
-			else												// IF Need to register account
-				chrome.tabs.create({url:"login.html"});
-		});
- 	}
+  updateBadge(newLecture);
 };
 
-const msgListener = (request, sender, sendResponse)=>{
-	if(!request.msg){
-		console.log(request);
-		return false;
-	}
+const access = async () => {
+  const data = await reqBlackboard();
+  if (!data) return;
 
-	switch(request.msg){
-		case "getLecture":
-			chrome.storage.local.get(['lecture'], (result)=>{
-				sendResponse({"Lecture": result.lecture});
-			});
-			break;
+  // GET INFORMATIONS IF SET LECTURES
+  // OTHERWISE GET LECTURES AND URL
+  // THEN SHOW
+  if (isLoggedIn(data)) {
+    console.log("Logged In");
 
-		case "checkoutAlarm":
-			const title = request.title;
-			chrome.storage.local.get(["lecture"], function(result){
-				const updatedLecture = result.lecture.map((lecture)=>{
-					if(lecture.title === title)
-						return {
-							...lecture,
-							record: lecture.alarm
-						};
+    chrome.storage.local.set({ available: true });
 
-					else return lecture;
-				});
-				
-				chrome.storage.local.set({lecture: updatedLecture});
-				updateBadge(updatedLecture);
-			});
+    chrome.storage.local.get(["lecture"], result => {
+      if (result.lecture)
+        // IF lecture URL is set then Parse url
+        updateLectureAlarm(result.lecture);
+      else {
+        // IF lecture URL is not set
+        chrome.tabs.create({ url: kulmsURL });
+        chrome.tabs.create({ url: "set_lecture.html" });
+      }
+    });
+  } else {
+    // Login and Re-Access
+    console.log("Login Needed");
 
-			break;
+    chrome.storage.local.get(["account", "available"], async result => {
+      if (result.account && result.available) {
+        //IF Account is saved & Available, Use it
+        let account = result.account;
+        let id, pw;
 
-		case "refreshAlarm":
-			access();
-			sendResponse({});
+        id = account.id;
+        pw = CryptoJS.AES.decrypt(account.pw, id).toString(CryptoJS.enc.Utf8);
 
-			break;
+        const loggedIn = await loginBlackboard(id, pw);
 
-		case "setLecture":
-			const lectureURLs = request.LectureURL;
-			const defaultLecture = {
-				"title": "",
-				"URL": {
-					"assignment": "",
-					"notice": "",
-					"material": ""
-				},
-				"record": {
-					"assignment": 0,
-					"notice": 0,
-					"material": 0
-				},
-				"alarm": {
-					"assignment": 0,
-					"notice": 0,
-					"material": 0
-				}
-			};
+        if (loggedIn) access();
+        else chrome.tabs.create({ url: "login.html" });
+      } // IF Need to register account
+      else chrome.tabs.create({ url: "login.html" });
+    });
+  }
+};
 
-			chrome.storage.local.get(["lecture"], function(result){
-				const orgLectures = result.lecture;
+const msgListener = (request, sender, sendResponse) => {
+  if (!request.msg) {
+    console.log(request);
+    return false;
+  }
 
-				const newLecture = lectureURLs.map((lecture)=>{
-					if(orgLectures && orgLectures.findIndex(orgLecture => orgLecture.title === lecture.title) !== -1){
-						const idx = orgLectures.findIndex(orgLecture => orgLecture.title === lecture.title);
-						orgLectures[idx].URL = lectureURLS.URL;
+  switch (request.msg) {
+    case "getLecture":
+      chrome.storage.local.get(["lecture"], result => {
+        sendResponse({ Lecture: result.lecture });
+      });
+      break;
 
-						return orgLectures[idx];
-					}
-					else{
-						const newLec = {
-							...defaultLecture,
-							"title": lecture.title,
-							"URL": lecture.URL
-						};
+    case "checkoutAlarm":
+      const title = request.title;
+      chrome.storage.local.get(["lecture"], function(result) {
+        const updatedLecture = result.lecture.map(lecture => {
+          if (lecture.title === title)
+            return {
+              ...lecture,
+              record: lecture.alarm
+            };
+          else return lecture;
+        });
 
-						return newLec;
-					}
-				});
+        chrome.storage.local.set({ lecture: updatedLecture });
+        updateBadge(updatedLecture);
+      });
 
-				chrome.storage.local.set({lecture: newLecture});
+      break;
 
-				sendResponse({});
+    case "refreshAlarm":
+      access();
+      sendResponse({});
 
-				access();
-			});
+      break;
 
-			break;
+    case "setLecture":
+      const lectureURLs = request.LectureURL;
+      const defaultLecture = {
+        title: "",
+        URL: {
+          assignment: "",
+          notice: "",
+          material: ""
+        },
+        record: {
+          assignment: 0,
+          notice: 0,
+          material: 0
+        },
+        alarm: {
+          assignment: 0,
+          notice: 0,
+          material: 0
+        }
+      };
 
-		case "editLecture":
-			chrome.tabs.create({url:"set_lecture.html"});
-			sendResponse({});
-			break;
+      chrome.storage.local.get(["lecture"], function(result) {
+        const orgLectures = result.lecture;
 
-		case "saveAccount":
-			const account = {
-				id: request.id,
-				pw: CryptoJS.AES.encrypt(request.pw, request.id)
-			};
+        const newLecture = lectureURLs.map(lecture => {
+          // 이미 등록된 강의
+          if (
+            orgLectures &&
+            orgLectures.findIndex(
+              orgLecture => orgLecture.title === lecture.title
+            ) !== -1
+          ) {
+            const idx = orgLectures.findIndex(
+              orgLecture => orgLecture.title === lecture.title
+            );
+            orgLectures[idx].URL = lecture.URL;
 
-			chrome.storage.local.set({account: account});
+            return orgLectures[idx];
+          }
+          // 새로 등록된 강의
+          else {
+            const newLec = {
+              ...defaultLecture,
+              title: lecture.title,
+              URL: lecture.URL
+            };
 
-			loginBlackboard(request.id, request.pw).then((loggedIn)=>{
-				sendResponse({loggedIn: loggedIn});
+            return newLec;
+          }
+        });
 
-				if(loggedIn)
-					access();
+        chrome.storage.local.set({ lecture: newLecture });
 
-				return true;
-			});
-			
-			break;
+        sendResponse({});
 
-		default: return false;
-	}
+        access();
+      });
 
-	return true;
+      break;
+
+    case "editLecture":
+      chrome.tabs.create({ url: "set_lecture.html" });
+      sendResponse({});
+      break;
+
+    case "saveAccount":
+      const account = {
+        id: request.id,
+        pw: CryptoJS.AES.encrypt(request.pw, request.id)
+      };
+
+      chrome.storage.local.set({ account: account });
+
+      loginBlackboard(request.id, request.pw).then(loggedIn => {
+        sendResponse({ loggedIn: loggedIn });
+
+        if (loggedIn) access();
+
+        return true;
+      });
+
+      break;
+
+    default:
+      return false;
+  }
+
+  return true;
 };
 
 init();
--- popup.js
+++ popup.js
@@ -1,50 +1,67 @@
-const loadAlarm = ()=>{
-	const clone = $("#entry_clone").clone().removeAttr("id");
-	const wrapper = $("#wrapper");
+const loadAlarm = () => {
+  const clone = $("#entry_clone")
+    .clone()
+    .removeAttr("id");
+  const wrapper = $("#wrapper");
 
-	chrome.runtime.sendMessage({msg: "getLecture"}, (response)=>{
-		if(!response.Lecture) return;
+  chrome.runtime.sendMessage({ msg: "getLecture" }, response => {
+    if (!response.Lecture) return;
 
-		const lectures = response.Lecture;
+    const lectures = response.Lecture;
 
-		wrapper.empty();
-		wrapper.css("height", `${lectures.length * 60 + 5}px`);
-		$("body").css("height", `${lectures.length * 60 + 20}px`);
+    wrapper.empty();
+    wrapper.css("height", `${lectures.length * 60 + 5}px`);
+    $("body").css("height", `${lectures.length * 60 + 20}px`);
 
-		lectures.forEach((entry)=>{
-			let elem = clone.clone();
+    lectures.forEach(entry => {
+      console.log(entry);
+      let elem = clone.clone();
 
-			elem.attr("title", entry.title);
-			elem.children(".title").text(entry.title);
-			for(let type in entry.alarm){
-				const alarmNum = entry.alarm[type] - entry.record[type];
-				elem.children(`.${type}`).attr("href", entry.URL[type]);
-				elem.children(`.${type}`).children(".number").text(`${alarmNum}`);
-				if(alarmNum > 0) 
-					elem.children(`.${type}`).children(".number").css("color", "#cc0000");
-			}
-			wrapper.append(elem);
-		});
-	});
-}
+      elem.attr("title", entry.title);
+      elem.children(".title").text(entry.title);
+      for (let type in entry.alarm) {
+        const alarmNum = entry.alarm[type] - entry.record[type];
+        elem
+          .children(`.${type}`)
+          .attr("href", entry.URL[type])
+          .removeClass("empty");
+        elem
+          .children(`.${type}`)
+          .children(".number")
+          .text(`${alarmNum}`);
+        if (alarmNum > 0)
+          elem
+            .children(`.${type}`)
+            .children(".number")
+            .css("color", "#cc0000");
+      }
+      wrapper.append(elem);
+    });
+  });
+};
 
 (function() {
-	loadAlarm();
+  loadAlarm();
 
-	$("#add").on("click", function(){
-		chrome.runtime.sendMessage({msg: "editLecture"}, (response)=>{
-			close();
-		});
-	});
+  $("#add").on("click", function() {
+    chrome.runtime.sendMessage({ msg: "editLecture" }, response => {
+      close();
+    });
+  });
 
-	$("#refresh").on("click", function(){
-		chrome.runtime.sendMessage({msg: "refreshAlarm"}, (response)=>{
-			loadAlarm();
-		});
-	});
+  $("#refresh").on("click", function() {
+    chrome.runtime.sendMessage({ msg: "refreshAlarm" }, response => {
+      loadAlarm();
+    });
+  });
 
-	$("#wrapper").on("click", ".entry .element", function(){
-		chrome.tabs.create({url: $(this).attr("href")});
-		chrome.runtime.sendMessage({msg: "checkoutAlarm", title: $(this).closest(".entry").attr("title")});
-	});
+  $("#wrapper").on("click", ".entry .element:not(.empty)", function() {
+    chrome.tabs.create({ url: $(this).attr("href") });
+    chrome.runtime.sendMessage({
+      msg: "checkoutAlarm",
+      title: $(this)
+        .closest(".entry")
+        .attr("title")
+    });
+  });
 })();
